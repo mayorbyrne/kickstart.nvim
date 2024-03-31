e0fb0db67abc48991dff487522eec4db2f9f1761
mayorbyrne <kmoritz@gmail.com>
2024-03-31 16:07:37 -0500

    autosave; dashboard

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+
+  'pocco81/auto-save.nvim',
+
+  {
+    'nvimdev/dashboard-nvim',
+  },
+}
