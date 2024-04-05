f285a9cc3f12c7beaa972054af2f88ac4074655c
Kevin Moritz <kevin@quavered.com>
2024-04-05 15:07:34 -0500

    put termguicolors in bufferline config

--- lua/custom/plugins/pc.lua
+++ lua/custom/plugins/pc.lua
@@ -15,6 +15,7 @@ return {
   { 'github/copilot.vim' },
   { 'akinsho/bufferline.nvim', version = '*', dependencies = 'nvim-tree/nvim-web-devicons', 
     config = function()
+      vim.opt.termguicolors = true
       require('bufferline').setup {
         options = {
           diagnostics = "nvim_lsp",
