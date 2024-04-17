80167e59ee0db5eee9f3418ad07f62be2fbbb7fa
Kevin Moritz <kevin@quavermusic.com>
2024-04-17 08:36:31 -0500

    remove dupes

--- lua/custom/plugins/mac.lua
+++ lua/custom/plugins/mac.lua
@@ -38,7 +38,6 @@ return {
     'nvim-telescope/telescope-project.nvim',
   },
   {
-
     'lukas-reineke/indent-blankline.nvim',
     main = 'ibl',
     opts = {
@@ -182,15 +181,6 @@ header = vim.split([[
   },
   {
     "nvim-tree/nvim-web-devicons",
-  },
-  {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
   },
   {
     "folke/trouble.nvim",
