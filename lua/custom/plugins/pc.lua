98846b0bda84b18005d9b77e2f1fa460d2627b87
mayorbyrne <kmoritz@gmail.com>
2024-04-21 20:27:40 -0500

    chore: update pc plugins

--- lua/custom/plugins/pc.lua
+++ lua/custom/plugins/pc.lua
@@ -184,4 +184,30 @@ header = vim.split([[
       require('mini.map').setup({})
     end,
   },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function ()
+      local harpoon = require('harpoon')
+      harpoon:setup({})
+
+      vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end,
+        { desc = "Add current file to harpoon" })
+      vim.keymap.set("n", "<leader>hq", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+        { desc = "Toggle harpoon list" })
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end,
+        { desc = "Go to previous harpoon buffer" })
+      vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end,
+        { desc = "Go to next harpoon buffer" })
+    end,
+  },
+  {
+    'airblade/vim-rooter',
+    config = function()
+      vim.g.rooter_patterns = { '.git', '.gitignore', '.gitmodules', 'pubspec.yaml', 'package.json', 'CHANGELOG.md' }
+    end,
+  },
 }
